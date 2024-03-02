8200b80023a01dcefbeb442b33fed7e799fa3c52
harristheharris <anthonyharris7777@gmail.com>
2024-03-02 16:32:23 -0500

    Holy crap. This small amount of code has been the biggest emotional rollercoaster. But we are here now. All i have is a small proof of concept. First commit

---
+++ index.mjs
@@ -0,0 +1,29 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import generateMarkdown from './utils/generateMarkdown.mjs';
+import questPkg from './utils/userQuestions.mjs';
+import fsPkg from 'fs';
+
+const questions = questPkg;
+const fs = fsPkg
+
+// const generateMarkdown = import("./utils/generateMarkdown.mjs")
+// const {questions} = require('./utils/userQuestions.js')
+
+console.log(questions);
+console.log(generateMarkdown);
+
+// TODO: Create a function to initialize app
+function init() {
+inquirer
+    .prompt(questions)
+    .then(answers=> {
+        console.log(answers)
+        let template = generateMarkdown(answers)
+        fs.writeFileSync("sample.md", template)
+    })
+
+}
+
+// Function call to initialize app
+init();
---
+++ utils/userQuestions.mjs
@@ -0,0 +1,17 @@
+// TODO: Create an array of questions for user input
+
+const questions = [{
+    type: "list",
+    message: "What license do you want",
+    name: "license",
+    choices: ["MIT", "ISC", "No License"]
+},
+{
+    message: "Say hello please",
+    name: "hello node.js"
+}
+
+];
+
+
+export default questions
