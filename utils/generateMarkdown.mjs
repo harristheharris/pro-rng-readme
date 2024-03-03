aaa0a3d8ec80ce9bfaed502c6e6a70d4cde6a7a7
harristheharris <anthonyharris7777@gmail.com>
2024-03-02 19:14:42 -0500

    we have a proof of concept as well as most of the sections

--- utils/generateMarkdown.mjs
+++ utils/generateMarkdown.mjs
@@ -1,25 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//I just put the badge and link in one function. I have no idea why you would have them seperate tbh
+function renderLicenseBadge(license) {
+  if (license == "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  }
+
+  if (license == "ISC") {
+    return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
+  }
+
+  return ""
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if(license == "No License"){return ""}
+  if (license == "No License") { return "" }
   return `## License \n${renderLicenseBadge(license)}`
+}
+
+function renderProjectDescription(description){
+  return `## Description \n ${description}`
+}
+
+function renderProjectInstallation(installation){
+  return `## Installation \n ${installation}`
+}
+
+function renderProjectUsage(usage){
+  return `## Usage \n ${usage}`
+}
+
+function renderProjectCredits(credits){
+  return `## Credits \n ${credits}`
+}
+
+function renderProjectContact(contact){
+  return `## Contact Me \n ${contact}`
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderProjectDescription(data.description)}  
+${renderProjectInstallation(data.installation)}
+${renderProjectUsage(data.usage)}
+${renderProjectCredits(data.credits)}
 ${renderLicenseSection(data.license)}
+${renderProjectContact(data.contact)}
 
-## Contact Me
 
 `;
 }
--- utils/userQuestions.mjs
+++ utils/userQuestions.mjs
@@ -1,15 +1,43 @@
 // TODO: Create an array of questions for user input
 
-const questions = [{
+const questions = [
+{
+    type: "input",
+    message: "What is the project title:",
+    name: "title"
+},
+{
+    type: "input",
+    message: "Provide a short description  explaining the what, why, and how of this project. Keep in the mind the following: What was your motivation? Why did you build this project? What problem does it solve? What did you learn?",
+    name: "description"
+},
+{
+    type: "input",
+    message: "What are the steps required to install this project?:",
+    name: "installation"
+},
+{
+    type:"input",
+    message: "Provide instructions and examples for use. Add screenshots if necessary NOTE: images are not supported and will need to be manually added to the README after generation:",
+    name: "usage"
+},
+{
+    type: "input",
+    message: "List colloborators, if any, along with links to their GitHub profiles. If third party assets were used list them here as well:",
+    name: "credits"
+},
+{
     type: "list",
-    message: "What license do you want",
+    message: "What license did you use:",
     name: "license",
     choices: ["MIT", "ISC", "No License"]
 },
 {
-    message: "Say hello please",
-    name: "hello node.js"
+    type: "input",
+    message:"Where can you be contacted?:",
+    name: "contact"
 }
+
 
 ];
 
